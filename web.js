3f22e68bead3da86398b82540da802d3ea84a957
Charles Nichols <cjnicho@new-host-2.home>
2014-03-14 20:40:45 -0400

    init for demo

--- web.js
+++ web.js
@@ -3,10 +3,32 @@ var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 
+process.env.PWD = process.cwd();
+
+var status = [];
+
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-  res.send('Hello World 4!');
+app.get('/services/:id/:status', function(req, res) {
+	if(status[req.params.id]){
+		res.send('replace ' + req.params.id + ' status ' + req.params.status);
+	}else{
+		res.send('new for ' + req.params.id + ' status ' + req.params.status);
+	}
+	status[req.params.id] = req.params.status;
+
+});
+
+app.get('/services/:id', function(req, res) {
+	if(status[req.params.id]){
+		res.send(status[req.params.id]);
+	}else{
+		res.send('UNKNOWN');
+	}
+});
+
+app.configure(function(){
+  app.use('/', express.static(process.env.PWD + '/public'));
 });
 
 var port = Number(process.env.PORT || 5000);
